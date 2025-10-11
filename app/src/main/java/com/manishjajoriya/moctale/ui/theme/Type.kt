9f89039500250d5ac7847d614800c1bfc44a4b6d
Manish Jajoriya <manishjajoriya2005@gmail.com>
2025-10-11 18:37:59 +0530

    Added font and remove dynamic theme

--- app/src/main/java/com/manishjajoriya/moctale/ui/theme/Type.kt
+++ app/src/main/java/com/manishjajoriya/moctale/ui/theme/Type.kt
@@ -1,36 +1,41 @@
 package com.manishjajoriya.moctale.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.manishjajoriya.moctale.R
 
-// Set of Material typography styles to start with
-val Typography =
-    Typography(
-        bodyLarge =
-            TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
-            )
-        /* Other default text styles to override
-        titleLarge = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
-            fontSize = 22.sp,
-            lineHeight = 28.sp,
-            letterSpacing = 0.sp
-        ),
-        labelSmall = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Medium,
-            fontSize = 11.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.5.sp
-        )
-        */
+val Inter =
+    FontFamily(
+        Font(R.font.inter_black, FontWeight.Black),
+        Font(R.font.inter_extrabold, FontWeight.ExtraBold),
+        Font(R.font.inter_bold, FontWeight.Bold),
+        Font(R.font.inter_semibold, FontWeight.SemiBold),
+        Font(R.font.inter_medium, FontWeight.Medium),
+        Font(R.font.inter_regular, FontWeight.Normal),
+        Font(R.font.inter_light, FontWeight.Light),
+        Font(R.font.inter_extralight, FontWeight.ExtraLight),
+        Font(R.font.inter_thin, FontWeight.Thin),
     )
+
+val Typography =
+    Typography().run {
+      copy(
+          displayLarge = displayLarge.copy(fontFamily = Inter),
+          displayMedium = displayMedium.copy(fontFamily = Inter),
+          displaySmall = displaySmall.copy(fontFamily = Inter),
+          headlineLarge = headlineLarge.copy(fontFamily = Inter),
+          headlineMedium = headlineMedium.copy(fontFamily = Inter),
+          headlineSmall = headlineSmall.copy(fontFamily = Inter),
+          titleLarge = titleLarge.copy(fontFamily = Inter),
+          titleMedium = titleMedium.copy(fontFamily = Inter),
+          titleSmall = titleSmall.copy(fontFamily = Inter),
+          bodyLarge = bodyLarge.copy(fontFamily = Inter),
+          bodyMedium = bodyMedium.copy(fontFamily = Inter),
+          bodySmall = bodySmall.copy(fontFamily = Inter),
+          labelLarge = labelLarge.copy(fontFamily = Inter),
+          labelMedium = labelMedium.copy(fontFamily = Inter),
+          labelSmall = labelSmall.copy(fontFamily = Inter),
+      )
+    }
